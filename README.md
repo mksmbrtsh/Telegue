81ffee1390523361bb1ce0391242a4833be1460f
mksmbrtsh <mksmbrtsh@gmail.com>
2014-04-19 12:24:37 +0400

    integrate gstreamer to video received

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ For more info, go to: http://wiki.virt2real.ru/wiki/%D0%9A%D0%B0%D0%BA_%D1%81%D0
 2. Send commands to motors.
 3. Get status from virt2real: battery vol, wi-fi status.
 4. Change setup settings: ip-address, port and timeout. 
+5. Receive H.264 video, base at [gstreamer1.0](http://docs.gstreamer.com/x/WQAp)
 
 ### Usage
 * Scheme:
@@ -20,7 +21,42 @@ For more info, go to: http://wiki.virt2real.ru/wiki/%D0%9A%D0%B0%D0%BA_%D1%81%D0
 * Device with Android OS. Min version: Android 4.0 «Ice Cream Sandwich». Api level >=11.
 * Default settings: 192.168.1.128:1083 and 6000 ms timeout.
 ![http://max-imsblog.blogspot.ru/2014/04/telegue-android-app-for-virt2real.html](http://3.bp.blogspot.com/-KhhuHZzmPjw/U0rARzMnXQI/AAAAAAAAJQs/xuu_hQpsslo/s1600/Screenshot_2014-04-13-20-47-33.png)
+* On board virt2real console(for example, by putty) command to start video-streaming. IP - your virt2real address:
+```
+gst-launch -v v4l2src always-copy=false chain-ipipe=true ! \
+   video/x-raw-yuv,format='(fourcc)'NV12, width=640, height=480, framerate='(fraction)'30/1 ! \
+   dmaiaccel ! \
+   dmaienc_h264 ddrbuf=true encodingpreset=2 ratecontrol=4 targetbitrate=600000 ! \
+   rtph264pay !  queue ! \
+   udpsink port=8888 host=IP sync=false
+```
+* Copy your caps from log, and set it in '/jni/gst.c' app_function method.
+[Modify caps](http://stackoverflow.com/questions/18511765/gstreamer-in-android-udp-stream):
+'''
+udpsrc port=8888 caps=\"application/x-rtp, media=video, clock-rate=90000, encoding-name=H264, sprop-parameter-sets=\\\"J2QAFKwrQLj/LwDxImo\\\\=\\\\,KO4fLA\\\\=\\\\=\\\"\", payload=96\" ! ...
+'''
+without type conversion (***)
+
+## Build
+
+### requires
+1. Eclipse Java EE IDE with Android Development tools, SDK and NDK.
+2. [gstreamer-1.0-android-arm-1.2.3-release](http://gstreamer.freedesktop.org/data/pkg/android/1.2.3/)
+
+### settings
+C/C++ Build
+Build Variables:
+Name           | Type     | Value
+---------------|----------|-----------------------------------------------------
+GSTREAMER_ROOT | Directory| PATH TO YOUR gstreamer-1.0-android-arm-1.2.3-release
+NDK            | String   | PATH TO YOUR android-ndk-r9d
+Enviroment:
+Name                      | Value            | Origin
+--------------------------|------------------|------------
+GSTREAMER_NDK_BUILD_PATH  | ${NDK}           | USER:CONFIG
+GSTREAMER_SDK_ROOT_ANDROID| ${GSTREAMER_ROOT}| USER:CONFIG
 
 ### Project base
 1. autobahn-0.5.0.jar - The WebSocket Protocol support https://github.com/tavendo/AutobahnAndroid and jackson-core-asl-1.9.7.jar and jackson-mapper-asl-1.9.7.jar
 2. TrackJoystickView - to motor control. https://github.com/mksmbrtsh/TrackJoystickView
+3. [gstreamer1.0](http://gstreamer.com/)
--- src/maximsblog/blogspot/com/telegue/MainActivity.java
+++ src/maximsblog/blogspot/com/telegue/MainActivity.java
@@ -6,6 +6,8 @@ import maximsblog.blogspot.com.telegue.TrackJoystickView.OnTrackJoystickViewMove
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.gstreamer.GStreamer;
+
 
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -15,32 +17,68 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnTrackJoystickViewMoveListener {
+public class MainActivity extends Activity implements OnTrackJoystickViewMoveListener, SurfaceHolder.Callback {
 
 	String mWSuri;
 	private Virt2Real mVirt2real;
 	private IntentFilter mIntentFilter;
 	private long mTimeout;
+	// gstreamer
+	private native void nativeInit();     // Initialize native code, build pipeline, etc
+    private native void nativeFinalize(); // Destroy pipeline and shutdown native code
+    private native void nativePlay();     // Set pipeline to PLAYING
+    private native void nativePause();    // Set pipeline to PAUSED
+    private static native boolean nativeClassInit(); // Initialize native class: cache Method IDs for callbacks
+    private native void nativeSurfaceInit(Object surface);
+    private native void nativeSurfaceFinalize();
+    private long native_custom_data;      // Native code will use this to keep private data
+    private boolean is_playing_desired;   // Whether the user asked to go to PLAYING
+	
+
+    static {
+        System.loadLibrary("gstreamer_android");
+        System.loadLibrary("telegue");
+        nativeClassInit();
+    }
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// Initialize GStreamer and warn if it fails
+        try {
+            GStreamer.init(this);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            finish(); 
+            return;
+        }
 		setContentView(R.layout.activity_main);
 		getActionBar().setTitle("");
-		mVirt2real = App.virt2real;
+		SurfaceView sv = (SurfaceView) this.findViewById(R.id.surface_video);
+        SurfaceHolder sh = sv.getHolder();
+        sh.addCallback(this);
+        mVirt2real = App.virt2real;
+        // init video receive
+        is_playing_desired = mVirt2real.isConnected();    
+        nativeInit();
+        // init controls
 		TrackJoystickView joystick = (TrackJoystickView) findViewById(R.id.joystickView);
 		joystick.setOnTrackJoystickViewMoveListener(this, TrackJoystickView.DEFAULT_LOOP_INTERVAL);
-
+		// set receive msg from virt2real
 		mIntentFilter = new IntentFilter();
 		mIntentFilter.addAction(Virt2Real.CONNECT);
 		mIntentFilter.addAction(Virt2Real.DISCONNECT);
 		mIntentFilter.addAction(Virt2Real.ERROR);
 		mIntentFilter.addAction(Virt2Real.STATUS);
 		registerReceiver(mIntentReceiver, mIntentFilter);
+		// get saved settings
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		mWSuri = "ws://" + prefs.getString("ip", "192.168.1.128") + ":" + prefs.getString("port", "1083");
 		mTimeout = Long.parseLong(prefs.getString("timeout", "6000"));
@@ -92,6 +130,7 @@ public class MainActivity extends Activity implements OnTrackJoystickViewMoveLis
 			if (m.equals(Virt2Real.CONNECT)) {
 				invalidateOptionsMenu();
 				getActionBar().setTitle("");
+				nativePlay();
 			}
 			if (m.equals(Virt2Real.DISCONNECT)) {
 				invalidateOptionsMenu();
@@ -99,11 +138,13 @@ public class MainActivity extends Activity implements OnTrackJoystickViewMoveLis
 				String s = intent.getStringExtra(Virt2Real.DISCONNECT);
 				if(s!=null)
 					Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+				nativePause();
 			}
 			if (m.equals(Virt2Real.ERROR)) {
 				invalidateOptionsMenu();
 				getActionBar().setTitle("");
 				Toast.makeText(MainActivity.this, "connection error", Toast.LENGTH_SHORT).show();
+				nativePause();
 			}
 			if (m.equals(Virt2Real.STATUS)) {
 				JSONObject j;
@@ -137,4 +178,47 @@ public class MainActivity extends Activity implements OnTrackJoystickViewMoveLis
 				}
 	}
 
+    protected void onDestroy() {
+        nativeFinalize();
+        super.onDestroy();
+    }
+
+    // Called from native code. This sets the content of the TextView from the UI thread.
+    private void setMessage(final String message) {
+    	final Activity cur = this;
+    	runOnUiThread (new Runnable() {
+    		public void run() {
+    			Log.i ("GStreamer", "message:" + message);
+    		}
+    	});
+    }
+
+    // Called from native code. Native code calls this once it has created its pipeline and
+    // the main loop is running, so it is ready to accept commands.
+    private void onGStreamerInitialized () {
+        Log.i ("GStreamer", "Gst initialized. Restoring state, playing:" + is_playing_desired);
+        // Restore previous playing state
+        if (is_playing_desired) {
+            nativePlay();
+        } else {
+            nativePause();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+            int height) {
+        Log.d("GStreamer", "Surface changed to format " + format + " width "
+                + width + " height " + height);
+        nativeSurfaceInit (holder.getSurface());
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d("GStreamer", "Surface created: " + holder.getSurface());
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d("GStreamer", "Surface destroyed");
+        nativeSurfaceFinalize ();
+    }
+	
 }
